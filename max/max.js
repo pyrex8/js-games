e5ce2d85fdaf3cc963b355504856199b7db3e5c6
pyrex8 <ev64bug@yahoo.com>
2024-03-21 20:09:32 -0700

    single ball and two boxes

--- max/max.js
+++ max/max.js
@@ -34,13 +34,45 @@ function Ball(x, y, r) {
 
   this.show = function() {
     var pos = this.body.position;
+
+    push();
     stroke(255);
-    strokeWeight(5);
+    strokeWeight(2);
     fill(255, 255, 255, 100);
+    translate(pos.x, pos.y);
     rectMode(CENTER);
-    circle(pos.x, pos.y, this.r);
+    circle(0, 0, this.r);
+    pop();
   }
 }
+
+function Box(x, y, w, h) {
+  var options = {
+    friction: 0.5,
+    restitution: 0.8,
+    angle: PI
+  }
+  this.body = Bodies.rectangle(x, y, w, h, options);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
+
+  this.show = function() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    stroke(255);
+    strokeWeight(2);
+    fill(255, 255, 255, 100);
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0, 0, this.w, this.h);
+    pop();
+  }
+}
+
 
 function mouseDragged() {
   playA4();
@@ -61,17 +93,24 @@ function setup() {
   ground = Bodies.rectangle(width/2, height/2, width/2, height/40, options);
   World.add(world, ground);
 
-  ball = new Ball(width/2, 0, 10);
+  boxes.push(new Box(300, 0, 40, 40));
+  boxes.push(new Box(500, 0, 40, 40));
+  ball = new Ball(width/2, 0, 20);
 
   synth.chain(Tone.Destination);
 }
 
 function draw() {
   background(10);
+  for (var i = 0; i < boxes.length; i++)
+  {
+      boxes[i].show();
+  }
+
   ball.show();
   stroke(255);
   strokeWeight(5);
   rectMode(CENTER);
-  rect(width/2, height/2, width/2, height/40);
+  rect(width/2, height/2, width/2, 20);
 }
 
